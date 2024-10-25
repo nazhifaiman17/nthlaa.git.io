73c2032caa5b7022fe02b3d75fb2db3e36cf687f
nazhifaiman17 <nazhifaiman881731@gmail.com>
2024-10-26 02:22:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def quiz():
+  print("Selamat datang ke Mini Quiz! 🎉")
+  print("Jawab soalan berikut:\n")
+
+  # Question 1
+  answer1 = input("1. Haiwan apa yang aku sayang? ")
+  if answer1.lower() == "senggaaa":
+      print("Betul! 🎉")
+  else:
+      print("Salah! Jawapan yang betul adalah 'SENGGaaa'.")
+
+  # Wish message
+  print("\nHai Seng, aku sayang kau banyak-banyak, aku rinduuu, aku rinduu sangat-sangat hm ❤️")
+
+# Run the quiz
+quiz()
